2f3e371675ad7405c3cf02c1a567b075b5558aed
Tony Branquinho <antony.branquinho@gmail.com>
2022-04-18 17:15:34 -0300

    Removendo pessoa com JPA

--- src/main/java/aplicaçao/Programa.java
+++ src/main/java/aplicaçao/Programa.java
@@ -10,18 +10,16 @@ public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Maria", "maria@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Joao", "joao@gmail.com");
-
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 
+		Pessoa p = em.find(Pessoa.class, 2);
 		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+		em.remove(p);
 		em.getTransaction().commit();
-		System.out.println("pronto");
+		
+		System.out.println("Pronto");
+		em.close();
+		emf.close();
 	}
 }
